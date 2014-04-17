6b3720879f244878f577acd03677ca99c98f839b
Ismo Haataja <ismo.haataja@digia.com>
2014-04-17 09:52:24 +0300

    Fixed gerrit email verification link requires insecure login
    When registering a new email address with gerrit, it sends an email
    with a verification link. This link now uses 'https:' schema instead
    of 'http:'.
    
    Task-number: QTQAINFRA-167
    Originally-by: Peter Heubeck <peter.heubeck@tieto.com>
    Change-Id: I0e2e5a88171d6d6519149f5a2c2248810436c156
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    Reviewed-by: Ismo Haataja <ismo.haataja@digia.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/RegisterNewEmailSender.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/RegisterNewEmailSender.java
@@ -1,4 +1,5 @@
 // Copyright (C) 2009 The Android Open Source Project
+// Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -55,6 +56,15 @@ public class RegisterNewEmailSender extends OutgoingEmail {
   @Override
   protected void format() throws EmailException {
     appendText(velocifyFile("RegisterNewEmail.vm"));
+  }
+
+  @Override
+  public String getGerritUrl() {
+    String url = args.urlProvider.get();
+    if (url.startsWith("http:")) {
+      url = "https:" + url.substring(5);
+    }
+    return url;
   }
 
   public String getUserNameEmail() {
